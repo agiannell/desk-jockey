ef9803f7c120e1f902a5857d5fabc761d4c65887
shoddyshaman <djtiwari90@gmail.com>
2021-02-17 18:13:52 -0700

    updating biolerplate

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
+import react from 'react';
+import { Link } from "react-router-dom";
+
 
 const Header = (props) => {
   return (
     <div>
-      Hello World! This is the Header Page.
+      <p></p>
     </div>
   )
 }
