e5d8ef185d0da012dd7af92910719bd2901907d8
shoddyshaman <djtiwari90@gmail.com>
2021-02-22 16:35:04 -0700

    playlist creation,updated database,room,playlist

--- src/components/room/Room.js
+++ src/components/room/Room.js
@@ -1,29 +1,49 @@
-import { useEffect } from 'react';
-import {connect} from 'react-redux';
+import { useEffect, useState } from 'react';
+import { connect} from "react-redux";
 import SpotifyPlayer from 'react-spotify-web-playback';
-import {setUserPlaylists} from '../../ducks/reducer/userReducer';
+import Playlist from '../playlist/playlist'
 
 const Room = (props) => {
-  const {accessToken, setUserPlaylists, user} = props;
-useEffect(() => {
-  fetch(`https://api.spotify.com/v1/users/${user.id}/playlists`, {
+  const {accessToken,  user} = props;
+  const [userPlaylists,setUserPlaylists] = useState([]);
+  const [showPlaylists,setShowPlaylists] = useState(false);
+  const [roomQueue,setRoomQueue] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.spotify.com/v1/users/${user.id}/playlists`, {
     headers: { Authorization: "Bearer " + accessToken },
   })
     .then((playList) => playList.json())
     .then((data) => {
-      console.log(user.id)
       setUserPlaylists(data);
-      console.log(data);
+      console.log(userPlaylists);
     });
-}, []); 
+  },[])
+  console.log(user)
+
+  const handleAddTrack = (uri) => {
+      setRoomQueue(...roomQueue,uri)
+  }
 
   return (
     <div>
+      <button onClick={() => setShowPlaylists(!showPlaylists)}>Playlists</button>
+      {showPlaylists ? (
+        <div>
+          {userPlaylists.items.map(playlist => (
+            <Playlist 
+            id={playlist.id}
+            name={playlist.name}
+            accessToken={accessToken}
+             />
+          ))}
+        </div>
+      ): null}
       <section>
         <SpotifyPlayer
           token={accessToken}
           uris={['spotify:playlist:5f03s8ZslD2guGAXaPNCSg']}
-          />;
+          />
       </section>
     </div>
   )
@@ -31,10 +51,10 @@ useEffect(() => {
 
 const mapStateToProps = (reduxState) => {
   return {
+    user: reduxState.userReducer.user,
     accessToken: reduxState.userReducer.accessToken,
-    userPlaylists: reduxState.userReducer.userPlaylists,
-    user: reduxState.userReducer.user
   };
 };
 
-export default connect(mapStateToProps, {setUserPlaylists})(Room);
+
+export default connect(mapStateToProps)(Room);
