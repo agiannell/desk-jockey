9687d30ce599f594e91e8a5599b5040874976fbe
keplerridge <kepler.ridge@gmail.com>
2021-02-22 16:42:57 -0700

    added some styling to the player

--- src/components/room/Room.js
+++ src/components/room/Room.js
@@ -5,6 +5,7 @@ import {setUserPlaylists} from '../../ducks/reducer/userReducer';
 
 const Room = (props) => {
   const {accessToken, setUserPlaylists, user} = props;
+
 useEffect(() => {
   fetch(`https://api.spotify.com/v1/users/${user.id}/playlists`, {
     headers: { Authorization: "Bearer " + accessToken },
@@ -18,12 +19,21 @@ useEffect(() => {
 }, []); 
 
   return (
-    <div>
-      <section>
+    <div className='page-parent'>
+      <section className='player-section'>
         <SpotifyPlayer
+          className='player'
           token={accessToken}
           uris={['spotify:playlist:5f03s8ZslD2guGAXaPNCSg']}
-          />;
+          styles={{
+            bgColor: '#160F29',
+            sliderColor: '#246A73',
+            color: '#F3DFC1',
+            trackNameColor: '#F3DFC1',
+            loaderColor: '#246A73',
+            activeColor: 'red'
+          }}
+          />
       </section>
     </div>
   )
