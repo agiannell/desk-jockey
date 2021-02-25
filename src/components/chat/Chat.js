e8ded55df03c1163f5eb4e868d1121efc3b60102
Jase-Lawson <JMLawsonOther@gmail.com>
2021-02-25 14:39:36 -0700

    added sockets for chat in room

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,33 +1,59 @@
 import { connect } from "react-redux";
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
-import Header from '../header/Header'
+import { useParams } from "react-router-dom";
 
 const Chat = (props) => {
-  const [socket,setSocket] = useState(null)
+  const [socket, setSocket] = useState(null)
+  const [messages, setMessages] = useState([])
+  const { id } = useParams()
+  const [message, setMessage] = useState('')
+  const { username } = props
 
   useEffect(() => {
-    if(!socket){
+    if (!socket) {
       setSocket(io.connect('http://localhost:4000'))
     } else {
-      socket.on('user-joined',({username}) => {
+      socket.on('user-joined', ({ username }) => {
         console.log(`${username} has joined the chat`)
+      })
+      socket.on('message', ({ message, username }) => {
+        setMessages((m) => {
+          return [...m, { message: message, username: username }]
+        })
       })
     }
     return () => {
-      if(socket){socket.disconnect()}
+      if (socket) { socket.disconnect() }
     }
-    
-    
-  },[socket])
+
+
+  }, [socket])
+
+  useEffect(() => {
+    if (socket) { socket.emit('join-room', { roomId: id, username: username }) }
+
+  }, [id, socket])
+
 
   return (
     <div>
-      <Header />
-      <button onClick={() => socket.emit('message',{ test: 'pizza'})}>Hit me</button>
-      <button onClick={() => socket.emit('join-room',{ roomId: 1, username: 'Ben'})}>join room</button>
+      <div>
+        {messages.map((el, id) => {
+          return <p key={id}>{el.message} - {el.username}</p>
+        })}
+      </div>
+      <form >
+        <input onChange={(e) => setMessage(e.target.value)} value={message} />
+        <button onClick={(e) => {
+          e.preventDefault()
+          socket.emit('message', { message, roomId: id, username })
+          setMessage('')
+        }}>Send</button>
+      </form>
     </div>
   )
 }
+
 
 export default Chat;
