acb1165ef527b15f36732561c7c1b2fede8912c3
shoddyshaman <djtiwari90@gmail.com>
2021-02-26 17:01:50 -0700

    updated player code

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,32 +1,52 @@
-import SpotifyPlayer from 'react-spotify-web-playback';
-import { connect } from 'react-redux';
+import SpotifyPlayer from "react-spotify-web-playback";
+import { connect } from "react-redux";
+import { useState, useEffect } from "react";
+var Spotify = require("spotify-web-api-js");
+var s = new Spotify();
 
-const Player = props => {
-  const { accessToken, desktopDjPL } = props
-  
+const Player = (props) => {
+  const { accessToken, desktopDjPL, playlistUri, getDesktopDjFn } = props;
+  const [playerUri,setPlayerUri] = useState(playlistUri)
+
+  useEffect(() => {}, []);
+
   return (
     <section>
       <SpotifyPlayer
-        className='player'
-        name='Desktop DJ Player'
+        callback={(state) => {
+          console.log(state);
+          if (state.progressMs === state.track.durationMs || !state.isPlaying) {
+            let djPlaylistId = playlistUri.substr(17,)
+            fetch(`https://api.spotify.com/v1/playlists/${djPlaylistId}`, {
+              headers: { Authorization: "Bearer " + accessToken },
+            })
+              .then((playList) => playList.json())
+              .then((data) => {
+                setPlayerUri(data.uri);
+              });
+          }
+        }}
+        className="player"
+        name="Desktop DJ Player"
         token={accessToken}
-        uris={desktopDjPL.map(e => e.track.uri)}
+        uris={playerUri}
         styles={{
-          bgColor: '#246A73',
-          sliderColor: '#246A73',
-          color: '#F3DFC1',
-          trackNameColor: '#F3DFC1',
-          loaderColor: '#246A73',
-          activeColor: 'red',
-          sliderHandleColor: '#F3DFC1',
-          sliderColor: '#F3DFC1'
-        }} />
+          bgColor: "#246A73",
+          sliderColor: "#246A73",
+          color: "#F3DFC1",
+          trackNameColor: "#F3DFC1",
+          loaderColor: "#246A73",
+          activeColor: "red",
+          sliderHandleColor: "#F3DFC1",
+          sliderColor: "#F3DFC1",
+        }}
+      />
     </section>
-  )
-}
+  );
+};
 
-const mapStateToProps = reduxState => ({
-  accessToken: reduxState.userReducer.accessToken
-})
+const mapStateToProps = (reduxState) => ({
+  accessToken: reduxState.userReducer.accessToken,
+});
 
-export default connect(mapStateToProps)(Player); 
+export default connect(mapStateToProps)(Player);
