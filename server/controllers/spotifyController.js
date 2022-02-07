88e6c37eebb457e0f14d3cada5b22f9e7f0bbdee
agiannell <agiannell@gmail.com>
2022-02-06 22:54:10 -0700

    update callack to only prod

--- server/controllers/spotifyController.js
+++ server/controllers/spotifyController.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 const request = require('request');
-const express = require('express');
-const app = express();
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, BASE_URL_DEV, REACT_APP_BASE_URL_PROD, REACT_APP_BASE_URL_DEV } = process.env;
-const env = app.settings.env
-
 
 module.exports = {
   spotifyLogin: (_req, res) => {
@@ -13,7 +9,7 @@ module.exports = {
       response_type: 'code',
       client_id: SPOTIFY_CLIENT_ID,
       scope: 'streaming user-read-private user-read-email user-read-playback-state user-modify-playback-state user-library-read user-library-modify playlist-read-private playlist-modify-public playlist-modify-private',
-      redirect_uri: (env !== "production" ? REACT_APP_BASE_URL_DEV : REACT_APP_BASE_URL_PROD) + '/callback'
+      redirect_uri: REACT_APP_BASE_URL_PROD + '/callback'
     }
     let spotifyAuthParams = new URLSearchParams(params);
     res.redirect('https://accounts.spotify.com/authorize?' + spotifyAuthParams.toString())
@@ -25,7 +21,7 @@ module.exports = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
-        redirect_uri: (env !== "production" ? REACT_APP_BASE_URL_DEV : REACT_APP_BASE_URL_PROD) + '/callback',
+        redirect_uri: REACT_APP_BASE_URL_PROD + '/callback',
         grant_type: 'authorization_code'
       },
       headers: {
@@ -39,7 +35,7 @@ module.exports = {
       // console.log(body)
       var access_token = body.access_token
       req.session.token = access_token
-      let uri = env !== 'production' ? BASE_URL_DEV : `${REACT_APP_BASE_URL_PROD}/` || (env !== "production" ? REACT_APP_BASE_URL_DEV : REACT_APP_BASE_URL_PROD) + '/callback'
+      let uri = `${REACT_APP_BASE_URL_PROD}/` || REACT_APP_BASE_URL_PROD + '/callback'
       res.redirect(uri)
     })
     // console.log(req.session.token)
