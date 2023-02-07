d1d6ae43046479d37e0e2501ea60f0368cede278
Anthony Giannell <agiannell@gmail.com>
2023-02-06 18:06:28 -0700

    updated sql schema with uuid

--- db/init.sql
+++ db/init.sql
@@ -1,37 +1,37 @@
-drop TABLE if exists room_junction, users, room;
+DROP TABLE IF EXISTS room_junction, users, room;
 
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    display_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    profile_pic TEXT NOT NULL,
+    user_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    display_name text NOT NULL,
+    email text NOT NULL,
+    profile_pic text NOT NULL,
     date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     playlist_uri text,
     UNIQUE (email)
 );
 
 CREATE TABLE room(
-    room_id SERIAL PRIMARY KEY,
-    room_name VARCHAR(55) NOT NULL,
-    room_pic TEXT,
-    password VARCHAR(255),
+    room_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    room_name text NOT NULL,
+    room_pic text,
+    password text,
     is_private boolean NOT NULL DEFAULT false,
     is_collaborative boolean NOT NULL DEFAULT false,
-    genre varchar(55),
-    description VARCHAR(255),
-    created_by int references users(user_id) on delete cascade
+    genre text,
+    description text,
+    created_by int REFERENCES users(user_id) on delete cascade
 );
 
 alter table room_junction add constraint room_junction_uq unique (room_id,user_id);
 
 create table room_junction (
-    junction_id SERIAL PRIMARY KEY,
-    room_id int references room(room_id) on delete cascade,
-    user_id int references users(user_id) on delete cascade
+    junction_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    room_id int REFERENCES room(room_id) on delete cascade,
+    user_id int REFERENCES users(user_id) on delete cascade
 );
 
 create table chat (
-    chat_id serial primary key,
-    content varchar(255),
-    room_id int references room(room_id) on delete cascade
+    chat_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    content text,
+    room_id int REFERENCES room(room_id) on delete cascade
 );
