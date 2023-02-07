758d21aebf7a842ca4d0d17a37978329926d16ee
Anthony Giannell <agiannell@gmail.com>
2023-02-06 18:08:12 -0700

    updated id reference fields from int to uuid

--- db/init.sql
+++ db/init.sql
@@ -19,19 +19,19 @@ CREATE TABLE room(
     is_collaborative boolean NOT NULL DEFAULT false,
     genre text,
     description text,
-    created_by int REFERENCES users(user_id) on delete cascade
+    created_by uuid REFERENCES users(user_id) on delete cascade
 );
 
 alter table room_junction add constraint room_junction_uq unique (room_id,user_id);
 
 create table room_junction (
     junction_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    room_id int REFERENCES room(room_id) on delete cascade,
-    user_id int REFERENCES users(user_id) on delete cascade
+    room_id uuid REFERENCES room(room_id) on delete cascade,
+    user_id uuid REFERENCES users(user_id) on delete cascade
 );
 
 create table chat (
     chat_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     content text,
-    room_id int REFERENCES room(room_id) on delete cascade
+    room_id uuid REFERENCES room(room_id) on delete cascade
 );
